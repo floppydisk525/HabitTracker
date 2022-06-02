713e50f93bb7743146f25044e346861fe0fd6f88
floppydisk525 <mark@ijohnsen.com>
2022-06-02 07:04:15 -0700

    install nuget package Microsoft.Data.sqlite

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Data.Sqlite;
+
+string connectionString = @"Data Source=habitTracker.db";
+
+using (var connection = new SqliteConnection(connectionString))
+{
+    connection.Open();
+    var tableCmd = connection.CreateCommand();
+    //create sql command to create database
+    tableCmd.CommandText = 
+    @"CREATE TABLE IF NOT EXISTS drinking_water(
+        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Date TEXT,
+        Quantity INTEGER
+        )";
+
+    tableCmd.ExecuteNonQuery();
+
+    connection.Close();
+}
