04df30f2e5ef51b6c3cd538f482c602fb5452e08
floppydisk525 <mark@ijohnsen.com>
2022-06-02 22:52:10 -0700

    started adding insert  methods as well as command line interface navigation.

--- Program.cs
+++ Program.cs
@@ -1,21 +1,104 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.Data.Sqlite;
 
-string connectionString = @"Data Source=habitTracker.db";
-
-using (var connection = new SqliteConnection(connectionString))
+namespace HabitTracker
 {
-    connection.Open();
-    var tableCmd = connection.CreateCommand();
-    //create sql command to create database
-    tableCmd.CommandText = 
-    @"CREATE TABLE IF NOT EXISTS drinking_water(
-        Id INTEGER PRIMARY KEY AUTOINCREMENT,
-        Date TEXT,
-        Quantity INTEGER
-        )";
-    
-    tableCmd.ExecuteNonQuery();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=habitTracker.db";
 
-    connection.Close();
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                //create sql command to create database
+                tableCmd.CommandText =
+                    @"CREATE TABLE IF NOT EXISTS drinking_water(
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Date TEXT,
+                    Quantity INTEGER
+                    )";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+
+            GetUserInput();
+        }
+
+        static void GetUserInput()
+        {
+            Console.Clear();
+            bool closeApp = false;
+            while (closeApp == false)
+            {
+                Console.WriteLine("\n\nMAIN MENU");
+                Console.WriteLine("\nWhat would you like to do?");
+                Console.WriteLine("\nType 0 to Close the Application.");
+                Console.WriteLine("Type 1 to View All Records.");
+                Console.WriteLine("Type 2 to Insert a Record.");
+                Console.WriteLine("Type 3 to Delete a Record.");
+                Console.WriteLine("Type 4 to Update a Record.");
+                Console.WriteLine("-----------------------------------------\n");
+
+                string commandInput = Console.ReadLine();
+
+                switch (commandInput)
+                {
+                    case "0":
+                        Console.WriteLine("\nGoodbye!\n");
+                        closeApp = true;
+                        break;
+                    //case 1:
+                    //    GetAllRecords();
+                    //    break;
+                    case "2":
+                        Insert();
+                        break;
+                        //case 3:
+                        //    Delete();
+                        //    break;
+                        //case 4:
+                        //    Update();
+                        //    break;
+                        //default:
+                        //    Console.WriteLine("\nInvalid Entry.  Please Enter an integer between 0 and 4.\n");
+                        //    break;
+                }
+            }
+        }
+        private static void Insert()
+        {
+            string date = GetDateInput();
+
+            int quantity = GetNumberInput("\n\nPlease insert numnber of glasses or other measure of your choice (no decimals allowed)\n\n");
+
+        }
+
+        internal static int GetNumberInput(string message)
+        {
+            Console.WriteLine(message);
+            string numberInput = Console.ReadLine();
+            if (numberInput == "0") GetUserInput();
+            int finalInput = Convert.ToInt32(numberInput);
+            return finalInput;
+        }
+
+        internal static string GetDateInput()
+        {
+            Console.WriteLine("\n\nPlease insert the date: (Format: dd-mm-yy).  Type 0 to return to the main menu.");
+
+            string dateInput = Console.ReadLine();
+
+            if (dateInput == "0") GetUserInput();
+
+            return dateInput;
+        }
+
+    }
+
+
 }
